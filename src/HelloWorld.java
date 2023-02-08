6e8aef7370eef8972ba87e5ee2da942e34ee7f08
FLS-IT <benjamin.daum@fls-wiesbaden.de>
2023-02-08 08:46:39 +0100

    Erster Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-            System.out.println("Hello world FLS-IT news !");
+            System.out.println("Hello world FLS-IT news  !");
         }
 
 }
